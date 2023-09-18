ae140be6ae4dbb2aea91a80f16ed4c501228ab5b
Niklas Rast <47035125+niklasrast@users.noreply.github.com>
2023-09-18 14:34:10 +0200

    Update INSTALL-LocalAdminWithPassword.ps1

--- INSTALL-LocalAdminWithPassword.ps1
+++ INSTALL-LocalAdminWithPassword.ps1
@@ -24,7 +24,8 @@ if ($true -ne (test-Path -Path "HKLM:\SOFTWARE\COMPANY")) {
 Start-Transcript -path $logFile
 
 $Username = "ladmin"
-$Password = "P444w0rd1234."
+#$Password = "P444w0rd1234."
+$Password = [System.Web.Security.Membership]::GeneratePassword(20,0)
 
 $group = (Get-WmiObject win32_group -filter "LocalAccount = $TRUE And SID = 'S-1-5-32-544'" | Select-Object -expand name)
 
